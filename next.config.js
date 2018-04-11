eea8ebe366e8268ae5db6c7df241538a40c2d5d2
Odin Ugedal <odin@ugedal.com>
2018-04-11 16:20:27 +0200

    Update next.js config

--- next.config.js
+++ next.config.js
@@ -10,40 +10,8 @@ module.exports = withCss({
     config.plugins = config.plugins.map(plugin => {
       if (plugin instanceof UglifyJSPlugin) {
         return new UglifyJSPlugin({
-          exclude: /(react\.js|__generated__)/,
-          parallel: true,
-          sourceMap: true,
-          uglifyOptions: {
-            compress: {
-              arrows: false,
-              booleans: false,
-              collapse_vars: false,
-              comparisons: false,
-              computed_props: false,
-              hoist_funs: false,
-              hoist_props: false,
-              hoist_vars: false,
-              if_return: false,
-              inline: false,
-              join_vars: false,
-              keep_infinity: true,
-              loops: false,
-              negate_iife: false,
-              properties: false,
-              reduce_funcs: false,
-              reduce_vars: false,
-              sequences: false,
-              side_effects: false,
-              switches: false,
-              top_retain: false,
-              toplevel: false,
-              typeofs: false,
-              unused: false,
-              conditionals: true,
-              dead_code: true,
-              evaluate: true
-            }
-          }
+          ...plugin.options,
+          sourceMap: true
         });
       }
       return plugin;
