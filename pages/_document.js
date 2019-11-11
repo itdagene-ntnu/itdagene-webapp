57aad0cd1bd7426887949a3dad94e95e9910e617
Ludvig Hozman <ludvig123@gmail.com>
2019-11-11 10:03:25 +0100

    Make all external links target blank

--- pages/_document.js
+++ pages/_document.js
@@ -135,6 +135,8 @@ export default class Default extends Document {
           <meta name="apple-mobile-web-app-title" content="itDAGENE" />
           <link rel="manifest" href="/static/manifest.json" />
 
+          <base target="_blank" />
+
           {this.props.styleTags}
 
           {GA_TRACKING_ID && (
