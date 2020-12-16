32bd446309834942d0bed952825e0b498e9bde5b
Ole August <oleastole@gmail.com>
2020-12-16 23:02:49 +0100

    fix: remove and somewhat update the page-queries

--- pages/stands/[id].tsx
+++ pages/stands/[id].tsx
@@ -1,40 +1,23 @@
 import * as React from 'react';
 import { graphql } from 'react-relay';
 import { withDataAndLayout, WithDataAndLayoutProps } from '../../lib/withData';
-import PageView from '../../components/PageView';
-import { program_QueryResponse } from '../../__generated__/program_Query.graphql';
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
 
 import * as _ from 'lodash';
+import Layout from '../../components/Layout';
 
-const DigitalStand = ({
-  error,
-  props,
-}: WithDataAndLayoutProps<program_QueryResponse>): JSX.Element => {
+const DigitalStand = (): JSX.Element => {
   const router = useRouter();
   const companyID = _.last(router.asPath.split('/'));
 
   return (
-    <>
-      {props.programPage && <PageView hideContent page={props.programPage} />}
+    <Layout noLoading responsive>
       {companyID}
-    </>
+    </Layout>
   );
 };
 
-export default withDataAndLayout(DigitalStand, {
-  query: graphql`
-    query program_Query {
-      programPage: page(slug: "program") {
-        ...PageView_page
-        ...metadata_metadata
-      }
-    }
-  `,
-  variables: {},
-  layout: ({ props, error }) => ({
-    responsive: true,
-    metadata: props && props.programPage,
-  }),
-});
+export default DigitalStand;
+
+// TODO: Add a query to the respective stand
