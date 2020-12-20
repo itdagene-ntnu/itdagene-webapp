85b663fa0b42ad8055ec32f404be08e5a54cf409
Ole August <oleastole@gmail.com>
2020-12-20 16:38:09 +0100

    feat: display the company's current event

--- pages/stands/index.tsx
+++ pages/stands/index.tsx
@@ -7,11 +7,22 @@ import styled from 'styled-components';
 import stands from '../../testing/companyMock';
 import StandCard from '../../components/Stands/StandCard';
 import { stands_QueryResponse } from '../../__generated__/stands_Query.graphql';
+import { useEffect, useState } from 'react';
+
+// Update the currentEvent-list every 30 sec
+const intervalLength = 1000 * 30
 
 const Index = ({
   error,
   props,
 }: WithDataAndLayoutProps<stands_QueryResponse>): JSX.Element => {
+  const [time, setTime] = useState(Date.now());
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(Date.now()), intervalLength);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       {props.stands && (
@@ -19,12 +30,15 @@ const Index = ({
       )}
 
       <StandGrid>
-        {stands.map((stand) => (
+        
+        {stands.map((stand, index) => (
           <StandCard
-            key={stand.id}
+            key={index}
+            // key={stand.id}
             active={stand.active}
             company={stand.company}
             id={stand.id}
+            time={time}
             // TODO: Decide if it's better to perform an independant query
             events={
               !props.events
