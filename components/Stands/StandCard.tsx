6f709834a12f20a5744be67e4da2c2fcf08e9a83
Ole August <oleastole@gmail.com>
2021-01-09 11:36:51 +0100

    feat: create HSP-standCard and resize on width < breakpoint

--- components/Stands/StandCard.tsx
+++ components/Stands/StandCard.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/router';
 import dayjs from 'dayjs';
 
 import customParseFormat from 'dayjs/plugin/customParseFormat';
-import { timeIsBetween } from '../../utils/time';
+import { timeIsBetween, timeIsAfter } from '../../utils/time';
 import { createFragmentContainer, graphql } from 'react-relay';
 import { StandCard_company } from '../../__generated__/StandCard_company.graphql';
 
@@ -30,12 +30,12 @@ const getCurrentEvent = (events: any, time: number) => {
   return currentEvent ?? null;
 };
 
-const eventTime = (event: any) => {
+const eventTime = (event: any, truncLength = 50) => {
   const timeRange = event
     ? `${event.timeStart.slice(0, 5)} - ${event.timeEnd.slice(0, 5)}`
     : '';
   const eventTitle = _.truncate(event ? event.title : '💁🏼‍♀️', {
-    length: 50,
+    length: truncLength,
   });
   return {
     timeRange,
@@ -46,15 +46,65 @@ const eventTime = (event: any) => {
 const StandCard = ({ company, time, type }: StandCardProps) => {
   const [currentEvent, setCurrentEvent] = useState();
   const router = useRouter();
+  const [width, setWidth] = React.useState(0);
+  const [shouldBreak, setShouldBreak] = React.useState(false);
 
+  useEffect(() => {
+    function onWidthChange(e) {
+      if (!shouldBreak && e.target.innerWidth <= 1199) {
+        setShouldBreak(true);
+      } else if (shouldBreak && e.target.innerWidth > 1199) {
+        setShouldBreak(false);
+      }
+    }
+    window.addEventListener('resize', onWidthChange);
+    return () => {
+      window.removeEventListener('resize', onWidthChange);
+    };
+  });
+  
+  useEffect(() => {
+    setCurrentEvent(() => getCurrentEvent(company.stand?.events ?? [], time));
+  }, [time]);
+  
   const handleRedirect = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.preventDefault();
     router.push(`/stands/[id]`, `/stands/${company.stand?.slug}`);
   };
 
-  useEffect(() => {
-    setCurrentEvent(() => getCurrentEvent(company.stand?.events ?? [], time));
-  }, [time]);
+  const renderHSPEvents = () => {
+    const relevantEvents = company.stand?.events.filter(
+      (event) => event != null && timeIsAfter(time, event.timeStart, event.date)
+    );
+
+    return currentEvent ? (
+      <>
+        <EventGrid>
+          <TimeSlot current={true}>
+            {eventTime(currentEvent).timeRange}
+          </TimeSlot>
+          <EventTitle>{eventTime(currentEvent, 200).eventTitle}</EventTitle>
+        </EventGrid>
+        {relevantEvents
+          ?.slice(Math.max(relevantEvents.length - 3, 0))
+          .map((event) => (
+            <EventGrid>
+              <TimeSlot>{eventTime(event).timeRange}</TimeSlot>
+              <EventTitle>{eventTime(event, 200).eventTitle}</EventTitle>
+            </EventGrid>
+          ))}
+      </>
+    ) : (
+      relevantEvents
+        ?.slice(Math.max(relevantEvents.length - 3, 0))
+        .map((event) => (
+          <EventGrid>
+            <TimeSlot>{eventTime(event).timeRange}</TimeSlot>
+            <EventTitle>{eventTime(event, 200).eventTitle}</EventTitle>
+          </EventGrid>
+        ))
+    );
+  };
 
   const CompanyCardContent = (
     <>
@@ -76,9 +126,28 @@ const StandCard = ({ company, time, type }: StandCardProps) => {
   );
 
   return type === 'hsp' ? (
-    <HSPContainer scale={1.03} onClick={handleRedirect}>
-      {CompanyCardContent}
-    </HSPContainer>
+    !shouldBreak ? (
+      <HSPContainer scale={1.03} onClick={handleRedirect}>
+        <HSPCompanyImgContainer>
+          <HSPCompanyImg src={company.logo ?? ''} />
+        </HSPCompanyImgContainer>
+        {/* <HSPCompanyInfo>
+        <SubHeader>{company.name}</SubHeader>
+        <CurrentEvent>
+          <TimeSlot>{eventTime(currentEvent).timeRange}</TimeSlot>
+          <EventTitle>{eventTime(currentEvent).eventTitle}</EventTitle>
+        </CurrentEvent>
+      </HSPCompanyInfo> */}
+        <FlexContainer>
+          <CompanyEvents>{renderHSPEvents()}</CompanyEvents>
+          <Live active={company.stand?.active ?? false} />
+        </FlexContainer>
+      </HSPContainer>
+    ) : (
+      <SPContainer scale={1.03} onClick={handleRedirect}>
+        {CompanyCardContent}
+      </SPContainer>
+    )
   ) : type === 'sp' ? (
     <SPContainer scale={1.03} onClick={handleRedirect}>
       {CompanyCardContent}
@@ -109,7 +178,6 @@ const StandardContainer = styled(NudgeDiv)`
   flex-direction: column;
   max-width: 100vw;
   height: 180px;
-
   background: #ffffff;
   box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.25);
   border-radius: 7px;
@@ -131,6 +199,13 @@ const SPContainer = styled(StandardContainer)`
 
 const HSPContainer = styled(SPContainer)`
   grid-column: -1/1;
+  display: grid;
+  grid-template-columns: 40% 60%;
+`;
+
+const CompanyEvents = styled.div`
+  align-self: center;
+  padding: 0 20px 0 20px;
 `;
 
 const FirstRow = styled.div`
@@ -146,17 +221,44 @@ const CompanyImgContainer = styled.div`
   width: 70%;
 `;
 
+const HSPCompanyImgContainer = styled.div`
+  display: flex;
+  padding: 20px;
+  border-right: 1.5px solid #f1f1f1;
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+`;
+
+const HSPEventListItem = styled.li`
+  list-style-type: none;
+`;
+
+const EventGrid = styled.div`
+  display: grid;
+  grid-template-columns: 20% 80%;
+  padding-bottom: 8px;
+`;
+
 const CompanyImg = styled.img`
   object-fit: contain;
   width: auto;
   height: 100%;
 `;
 
+const HSPCompanyImg = styled(CompanyImg)``;
+
 const CompanyInfo = styled.div`
   display: flex;
   height: 100%;
   flex-direction: column;
   justify-content: space-between;
+`;
+
+const HSPCompanyInfo = styled(CompanyInfo)`
+  padding-left: 20px;
+  border-right: 1.5px solid #f1f1f1;
 `;
 
 const CurrentEvent = styled.div`
@@ -169,8 +271,8 @@ const SubHeader = styled.span`
   font-weight: 600;
 `;
 
-const TimeSlot = styled.span`
-  font-weight: 200;
+const TimeSlot = styled.span<{ current?: boolean }>`
+  font-weight: ${(props) => (props.current ? 600 : 200)};
 `;
 
 const LiveContainer = styled.div<{ active: boolean }>`
@@ -178,18 +280,14 @@ const LiveContainer = styled.div<{ active: boolean }>`
   align-items: center;
   justify-content: center;
   background-color: #ffffff;
-
-  flex: 0 1 50px;
-
-  height: 40%;
-
+  flex: 0 0 50px;
+  height: 25px;
   padding: 4px;
-  border: 1px solid ${(props) => (props.active ? 'red' : 'grey')};
   border-radius: 3px;
-  color: ${(props) => (props.active ? 'red' : 'grey')};
   text-align: center;
-
   stroke: 2px;
+  border: 1px solid ${(props) => (props.active ? 'red' : 'grey')};
+  color: ${(props) => (props.active ? 'red' : 'grey')};
   text-decoration: ${(props) => (props.active ? 'none' : 'line-through')};
 `;
 
