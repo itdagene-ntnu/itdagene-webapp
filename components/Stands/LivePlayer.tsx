db4f497ba4392577f87981f5edc45d8d2a57feec
Ole August <oleastole@gmail.com>
2021-01-14 19:33:19 +0100

    feat: conditionally render button to /stands

--- components/Stands/LivePlayer.tsx
+++ components/Stands/LivePlayer.tsx
@@ -24,6 +24,7 @@ const Player = styled('div')`
   text-align: center;
   vertical-align: middle;
   position: relative;
+  margin-bottom: 50px;
   @media only screen and (max-width: 993px) {
     width: 100%;
     height: 400px;
