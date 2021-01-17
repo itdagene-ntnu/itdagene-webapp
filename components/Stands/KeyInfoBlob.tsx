f83ac915bd05622e2dc5e16aa019341e535c5f20
Ole August <oleastole@gmail.com>
2021-01-17 01:10:57 +0100

    fix: add dynamic truncation and make the badges rectangular

--- components/Stands/KeyInfoBlob.tsx
+++ components/Stands/KeyInfoBlob.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { createFragmentContainer, graphql } from 'react-relay';
 import styled from 'styled-components';
-
 import { KeyInfoBlob_keyInformation } from '../../__generated__/KeyInfoBlob_keyInformation.graphql';
+import {
+  itdageneLightBlue,
+  itdageneRed,
+  itdageneYellow,
+} from '../../utils/colors';
+import { EllipseOverflowDiv } from '../Styled';
 
 const KeyInfoContainer = styled.div`
   width: 200px;
+  height: 260px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: flex-end;
 `;
 
 const InfoKey = styled.h3`
   text-align: center;
+  margin: 0 0 12px 0;
 `;
 const InfoValue = styled.h3`
   font-size: 18px;
@@ -20,35 +28,40 @@ const InfoValue = styled.h3`
 `;
 
 const InfoCircle = styled.div`
-  --diameter: 170px;
+  --diameter: 190px;
 
   display: flex;
   justify-content: center;
   align-items: flex-start;
   height: var(--diameter);
   width: var(--diameter);
-  border-radius: 50%;
+  border-radius: 4%;
   box-shadow: 0px 0px 6px 1.5px rgba(0, 0, 0, 0.15);
 `;
 
 const IconContainer = styled.span<{ color: string }>`
   color: ${(props): string => props.color};
-  font-size: 3em;
+  font-size: 3.7em;
   margin: 0;
 `;
 
 const ValueInfoContainer = styled.div`
+  height: 80%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 30px;
+  margin: 10px 0;
 `;
 
-const InfoValueContainer = styled.div`
-  display: flex;
-  justify-content: center;
+const InfoValueContainer = styled(EllipseOverflowDiv)`
   text-align: center;
-  width: 150px;
+  margin: 0 10px;
+`;
+
+const FlexCenter = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
 `;
 
 type KeyType = 'it-ansatte' | 'hovedkontor' | 'bedrift-type' | 'ett-ord';
@@ -64,23 +77,23 @@ type KeyInfoType = {
 
 const keyInfoObject: KeyInfoType = {
   'it-ansatte': {
-    title: 'IT-ansatte',
-    color: '#ffb5a7',
+    title: 'Antall IT-ansatte',
+    color: itdageneYellow,
     icon: 'far fa-smile-wink',
   },
   hovedkontor: {
     title: 'Hovedkontor',
-    color: '#E01E5B99',
+    color: itdageneRed,
     icon: 'fas fa-building',
   },
   'bedrift-type': {
     title: 'Type bedrift',
-    color: '#43C0EB99',
+    color: itdageneLightBlue,
     icon: 'fas fa-briefcase',
   },
   'ett-ord': {
-    title: 'Utmerker seg selv ved',
-    color: '#f2cc8f',
+    title: 'Beskriver seg selv med ett ord:',
+    color: itdageneYellow,
     icon: 'fas fa-medal',
   },
 };
@@ -100,9 +113,11 @@ const KeyInfoBlob = ({ keyInfo }: KeyInfoProps): JSX.Element => {
           <IconContainer color={keyInfoObject[keyInfo.name as KeyType].color}>
             <i className={keyInfoObject[keyInfo.name as KeyType].icon} />
           </IconContainer>
-          <InfoValueContainer>
-            <InfoValue>{keyInfo.value}</InfoValue>
-          </InfoValueContainer>
+          <FlexCenter>
+            <InfoValueContainer maxLines={3}>
+              <InfoValue>{keyInfo.value}</InfoValue>
+            </InfoValueContainer>
+          </FlexCenter>
         </ValueInfoContainer>
       </InfoCircle>
     </KeyInfoContainer>
