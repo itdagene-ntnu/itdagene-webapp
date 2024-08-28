0e3ca6452e683ec9e4d13a252d1ae93adf521ae2
Isak Berg Endresen <isakbendresen@gmail.com>
2024-08-28 14:18:59 +0200

    Validate linkedin link

--- components/BoardMember.tsx
+++ components/BoardMember.tsx
@@ -20,41 +20,54 @@ type Props = {
   user: BoardMember_user;
 };
 
+/**
+ * linkedin field can be entire link (with or without https://) or username
+ * also ensure it is a valid linkedin link for it to show
+ * PS: consider moving this logic to backend
+ */
+const linkedInStr = (linkedin: string): string | null => {
+  let str = '';
+  if (!linkedin.includes('https://')) str += 'https://';
+  if (!linkedin.includes('www.linkedin.com/in/')) str += 'www.linkedin.com/in/';
+  str += linkedin;
+  const regex = /https:\/\/www\.linkedin\.com\/in\/[A-Za-z0-9.-]+/i;
+  return regex.test(str) ? str : null;
+};
+
 const BoardMember = ({
   user: { role, fullName, photo, email, linkedin },
-}: Props): JSX.Element => (
-  <Card>
-    <CenterIt text>
-      <RoundHead src={photo || ''} />
-      <Flex flexDirection="column">
-        <h4 style={{ fontSize: 15, marginBottom: 0 }}>{fullName}</h4>
-        <i>{role}</i>
-        <Flex justifyContent="center" alignItems="center" gap="10px">
-          <a
-            style={{ height: '40px', fontSize: '40px' }}
-            href={`mailto:${email}`}
-          >
-            {/* @ts-ignore*/}
-            <ion-icon name="mail" />
-          </a>
-          {linkedin && (
+}: Props): JSX.Element => {
+  const linkedInLink = linkedin ? linkedInStr(linkedin) : null;
+  return (
+    <Card>
+      <CenterIt text>
+        <RoundHead src={photo || ''} />
+        <Flex flexDirection="column">
+          <h4 style={{ fontSize: 15, marginBottom: 0 }}>{fullName}</h4>
+          <i>{role}</i>
+          <Flex justifyContent="center" alignItems="center" gap="10px">
             <a
-              style={{ height: '32px', fontSize: '32px' }}
-              href={`https://${
-                linkedin.includes('www.linkedin.com/in/')
-                  ? linkedin
-                  : `www.linkedin.com/in/${linkedin}`
-              }`}
+              style={{ height: '40px', fontSize: '40px' }}
+              href={`mailto:${email}`}
             >
               {/* @ts-ignore*/}
-              <ion-icon name="logo-linkedin" />
+              <ion-icon name="mail" />
             </a>
-          )}
+            {linkedInLink && (
+              <a
+                style={{ height: '32px', fontSize: '32px' }}
+                href={linkedInLink}
+              >
+                {/* @ts-ignore*/}
+                <ion-icon name="logo-linkedin" />
+              </a>
+            )}
+          </Flex>
         </Flex>
-      </Flex>
-    </CenterIt>
-  </Card>
-);
+      </CenterIt>
+    </Card>
+  );
+};
 
 export default createFragmentContainer(BoardMember, {
   user: graphql`
