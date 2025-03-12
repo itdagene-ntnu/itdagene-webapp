6e8fdc365acd628fe7d3d976fc94c7d86548950c
overmannen <jesper231@gmail.com>
2025-03-12 20:36:54 +0100

    fixed commenting

--- components/Joblistings/JoblistingView.tsx
+++ components/Joblistings/JoblistingView.tsx
@@ -30,7 +30,7 @@ const Sidebar = styled('div')`
   text-align: center;
   flex-direction: column;
   align-items: center;
-  #border-left: 1px solid ${lightGrey};
+  //border-left: 1px solid ${lightGrey};
   margin: 20px 20px 20px 20px;
   padding: 0 20px;
 `;
